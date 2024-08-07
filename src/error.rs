94e82163d8d22bc109d3dfb19968c10fd0bbfdba
Clivaly Cardoso <clivalydev@gmail.com>
2024-08-07 20:49:29 -0300

    Implement errors for server and client

--- src/error.rs
+++ src/error.rs
@@ -7,7 +7,7 @@ use axum::{
 
 pub type Result<T> = core::result::Result<T, CustomError>;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, strum_macros::AsRefStr)]
 pub enum CustomError {
     LoginFail,
 
@@ -24,14 +24,55 @@ impl IntoResponse for CustomError {
     fn into_response(self) -> Response {
         println!("->> {:<12} - {self:?}", "INTO_RES");
 
-        (StatusCode::INTERNAL_SERVER_ERROR, "UNHANDLED_CLIENT_ERROR").into_response()
+        // Create a placeholder axum response.
+        let mut response = StatusCode::INTERNAL_SERVER_ERROR.into_response();
+
+        // Insert the error into the response.
+        response.extensions_mut().insert(self);
+
+        response
     }
 }
 
-impl std::fmt::Display for CustomError {
-    fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(fmt, "{self:?}")
+// impl std::fmt::Display for CustomError {
+//     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+//         write!(fmt, "{self:?}")
+//     }
+// }
+
+// impl std::error::Error for CustomError {}
+
+impl CustomError {
+    pub fn client_status_and_error(&self) -> (StatusCode, ClientError) {
+        #[allow(unreachable_patterns)]
+        match self {
+            // -- Login
+            Self::LoginFail => (StatusCode::FORBIDDEN, ClientError::LOGIN_FAIL),
+
+            // -- Auth
+            Self::AuthFailNoAuthTokenCookie
+            | Self::AuthFailTokenWrongFormat
+            | Self::AuthFailCtxNotInRequestExt => (StatusCode::FORBIDDEN, ClientError::NO_AUTH),
+
+            // -- Model
+            Self::TicketDeleteFailIdNotFound { .. } => {
+                (StatusCode::BAD_REQUEST, ClientError::INVALID_PARAMS)
+            }
+
+            // -- Fallback.
+            _ => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                ClientError::SERVICE_ERROR,
+            ),
+        }
     }
 }
 
-impl std::error::Error for CustomError {}
+#[derive(Debug, strum_macros::AsRefStr)]
+#[allow(non_camel_case_types)]
+pub enum ClientError {
+    LOGIN_FAIL,
+    NO_AUTH,
+    INVALID_PARAMS,
+    SERVICE_ERROR,
+}
